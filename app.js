972285120cc8cfaac6a5ff375cfcc67345602d26
yideg <misganewendeg879@gmail.com>
2023-11-02 22:20:25 +0300

    feat:email send done.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const app = express();
 const dotenv =  require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 dotenv.config({
     encoding: 'utf8',
     debug: process.env.DEBUG||false,
     override: false,
 
 });
+var app = express();
 
 // Set up your Nodemailer transporter
-const to = "misganewendeg879@gmail.com";
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -20,30 +22,29 @@ const transporter = nodemailer.createTransport({
         pass: process.env.SENDER_EMAIL_PASSWORD,
     }
 });
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Define a route for sending emails
-app.post('/send-email', (req, res) => {
-  const mailOptions = {
-    from: process.env.SENDER_EMAIL_USERNAME,
-    to: 'recipient@example.com',
-    subject: 'Your Subject',
-    text: 'Your email message goes here.',
-    cc: ["yideg.misganew.3090@gmail.com"],
-    bcc:["yideg.misganew.3090@gmail.com"],
-
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
+app.post('/api/send-email', async(req, res) => {
+    const {sender,subject,message}= req.body;
+  await transporter.sendMail({
+    from: process.env.SENDER_EMAIL_USERNAME, // sender address
+    to: process.env.RECEIVER_EMAIL, // list of receivers
+    subject: subject, // Subject line
+    text: `New user ${sender} has sent you new message \n${message}`, // plain text body
+  }, (error, info) => {
     if (error) {
       console.log(error);
       res.status(500).send('Email not sent');
     } else {
       console.log('Email sent: ' + info.response);
-      res.send('Email sent');
+      res.status(200).send('Email sent');
     }
   });
 });
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+app.listen( process.env.PORT || 3000, () => {
+  console.log('Server is running');
 });
